ce6596ceef10907e4cc665a83bad9991acf3987d
rudneyrw <rudneyrw@gmail.com>
2025-06-17 14:52:51 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Taxi Fare Estimator</h1>
+    <p>This is your React app base.</p>
+  </React.StrictMode>
+);
